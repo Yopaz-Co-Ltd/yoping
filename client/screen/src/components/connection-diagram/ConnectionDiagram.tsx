3f56ea3d187799c765d5bcca8c5100411b6967dd
yopaz-hieunt <hieunt@yopaz.dev>
2025-07-11 14:12:31 +0700

    update: ui

--- client/screen/src/components/connection-diagram/ConnectionDiagram.tsx
+++ client/screen/src/components/connection-diagram/ConnectionDiagram.tsx
@@ -1,6 +1,7 @@
 import './connection-diagram.css'
 import globalIcon from '../../assets/global.svg'
 import routerIcon from '../../assets/router.png'
+import plugIcon from '../../assets/plug.svg'
 
 function ConnectionDiagram() {
   return (
@@ -10,20 +11,21 @@ function ConnectionDiagram() {
         </div>
         <div className='list-devices'>
             <div className='internet-connect-line'></div>
-            <div className='connect-line'></div>
             <div className="diagram-item router">
                 <img src={routerIcon} alt="Router" />
                 <span className='device-name'>Trên đầu BOD</span>
+                <div className='connect-line'></div>
             </div>
             <div>
-                LAN
+                <img src={plugIcon} width={14} height={14} alt="Ethernet" />
             </div>
             <div className="diagram-item router">
                 <img src={routerIcon} alt="Router" />
                 <span className='device-name'>Trên đầu web5</span>
+                <div className='connect-line'></div>
             </div>
             <div>
-                LAN
+                <img src={plugIcon} width={14} height={14} alt="Ethernet" />
             </div>
             <div className="diagram-item router">
                 <img src={routerIcon} alt="Router" />
