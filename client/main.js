771b1d20e0a690f6788741fd13e3597dacf0f1ee
yopaz-quanlv <quanlv@yopaz.vn>
2025-07-11 11:33:30 +0700

    tray mode

--- client/main.js
+++ client/main.js
@@ -1,23 +1,62 @@
 console.log('Hello from Electron 👋')
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Tray, Menu, screen } = require('electron')
 const path = require('path')
 
 const isDev = process.env.NODE_ENV === 'development'
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+let tray = null
+let win = null
 
-  if (isDev) {
-    win.loadURL('http://localhost:5173')
-  } else {
-    win.loadFile(path.join(__dirname, 'screen/dist/index.html'))
-  }
+const createTray = () => {
+  tray = new Tray(path.join(__dirname, 'yoping.png'))
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Quit', click: () => app.quit() }
+  ])
+  tray.setContextMenu(contextMenu)
+  tray.setToolTip('Yoping App')
+  tray.on('click', () => {
+    if (win) {
+      win.isVisible() ? win.hide() : win.show()
+    } else {
+      win = new BrowserWindow({
+        width: 375,
+        height: 420,
+        show: false,
+        frame: false,
+        transparent: true,
+        resizable: false,
+        alwaysOnTop: true,
+        skipTaskbar: true,
+        webPreferences: {
+          nodeIntegration: true
+        }
+      })
+      if (isDev) {
+        win.loadURL('http://localhost:5173')
+      } else {
+        win.loadFile(path.join(__dirname, 'screen/dist/index.html'))
+      }
+
+      win.on('blur', () => {
+        win.hide()
+      })
+
+      win.once('ready-to-show', () => {
+        const trayBounds = tray.getBounds()
+        const primaryDisplay = screen.getPrimaryDisplay()
+        const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (375 / 2))
+        const y = Math.round(trayBounds.y + trayBounds.height + 4)
+        win.setPosition(x, y)
+        win.show()
+      })
+    }
+  })
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  if (process.platform === 'darwin') {
+    app.dock.hide()
+  }
+  createTray()
 })
