cb8c60fa2330ac6771c46d3e9e62e13f3bbd0588
yopaz-hieunt <hieunt@yopaz.dev>
2025-07-18 14:39:31 +0700

    add: uuid

--- client/main.js
+++ client/main.js
@@ -2,12 +2,14 @@ const { app } = require('electron')
 const SystemEvent = require('./services/events')
 const { createTray } = require('./utils/setup')
 const { setUpIpcMain } = require('./utils/ipcMain')
+const { getDeviceUUID } = require('./utils/uuid')
 const systemEvent = new SystemEvent();
 
 setUpIpcMain();
 
 const init = () => {
   systemEvent.start();
+  getDeviceUUID();
 };
 
 app.whenReady().then(() => {
